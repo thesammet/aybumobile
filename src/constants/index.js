caf5d34cd778f69348a8836229251561cb42798f
Mertcan Kose <mertcankose97@outlook.com>
2022-10-16 20:35:22 +0300

    parse website with cheerio

--- src/constants/index.js
+++ src/constants/index.js
@@ -2,3 +2,5 @@ export const designWidth = 375;
 export const designHeight = 812;
 
 export const tabHeight = 70;
+export const aybuUrl =
+  'https://aybu.edu.tr/sks/tr/sayfa/6265/Ayl%C4%B1k-Yemek-Men%C3%BCs%C3%BC';
--- src/screens/Food.js
+++ src/screens/Food.js
@@ -1,11 +1,74 @@
-import {View, Text} from 'react-native';
-import React from 'react';
+import {View, Text, ScrollView} from 'react-native';
+import {useEffect, useState} from 'react';
+import {aybuUrl} from '../constants';
+import cheerio from 'cheerio-without-node-native';
+import moment from 'moment';
 
 const Food = () => {
+  const [dayAndMeal, setDayAndMeal] = useState([]);
+
+  useEffect(() => {
+    getFoodList();
+  }, []);
+
+  const getFoodList = async () => {
+    let response = await fetch(aybuUrl);
+    let htmlString = await response.text();
+    const $ = cheerio.load(htmlString);
+
+    let dayAndFoodList = []; // Can't distinguish between meals and dates because they are both in strong tag. Both are coming.
+    let dayList = [];
+    let foodList = [];
+
+    // Get all strong tags
+    $('.alert')
+      .find('strong')
+      .each((i, element) => {
+        let food = $(element)
+          .text()
+          .replace(/(\r\n|\n|\r)/gm, '')
+          .replace(/\s+/g, ' ')
+          .trim();
+        dayAndFoodList.push(food);
+      });
+
+    // Get all dates
+    dayAndFoodList.forEach((element, index) => {
+      if (moment(element, 'DD.MM.YYYY', true).isValid()) {
+        dayList.push(element);
+      }
+    });
+
+    // Get all meals
+    $('.alert')
+      .find('ul')
+      .each((i, element) => {
+        let food = $(element)
+          .text()
+          .replace(/(\r\n|\n|\r)/gm, '')
+          .replace(/\s+/g, ' ')
+          .trim();
+        foodList.push(food);
+      });
+
+    // combine dayList and foodList
+    let foodListWithDay = [];
+    dayList.forEach((element, index) => {
+      foodListWithDay.push({
+        day: element,
+        food: foodList[index],
+      });
+    });
+
+    setDayAndMeal(foodListWithDay);
+
+    console.log(foodListWithDay);
+  };
+
   return (
-    <View>
-      <Text>Food</Text>
-    </View>
+    <ScrollView>
+      <Text>{JSON.stringify(dayAndMeal, null, 2)}</Text>
+    </ScrollView>
   );
 };
 
