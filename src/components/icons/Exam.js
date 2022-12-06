14e74ee73948b8f6a3d644cc39b8f6f1e90330bd
Abdü Samed AKGÜL <50338990+thesammet@users.noreply.github.com>
2022-12-07 01:15:24 +0300

    addition: calendar

---
+++ src/components/icons/Exam.js
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import Svg, {Path} from 'react-native-svg';
+const SvgExam = props => (
+  <Svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 490 490"
+    style={{
+      enableBackground: 'new 0 0 490 490',
+    }}
+    xmlSpace="preserve"
+    accessibilityRole="image"
+    {...props}>
+    <Path d="M424.976 95c-.001-2.601-.992-5.159-2.904-7.071l-85-85c-1.912-1.912-4.47-2.904-7.071-2.905V0H75c-5.523 0-10 4.477-10 10v470c0 5.523 4.477 10 10 10h340c5.522 0 10-4.477 10-10V95h-.024zM340 34.142 390.857 85H340V34.142zM405 470H85V20h235v75c0 5.523 4.478 10 10 10h75v365z" />
+    <Path d="m259.097 295 22.445 51.624 18.342-7.975-70.713-162.637c-1.613-3.71-5.257-6.054-9.345-6.011a10 10 0 0 0-9.13 6.333l-65 165 18.608 7.33L185.445 295h73.652zm-38.641-88.873L250.4 275h-57.077l27.133-68.873zM315 165h-20v20h-20v20h20v20h20v-20h20v-20h-20zM110 410h40v20h-40zM170 410h40v20h-40zM230 410h150v20H230zM110 40h40v20h-40zM110 80h80v20h-80z" />
+  </Svg>
+);
+export default SvgExam;
--- src/screens/Calendar.js
+++ src/screens/Calendar.js
@@ -14,23 +14,28 @@ import { ProfileContext } from '../context/Profile';
 import { ThemeContext } from '@/context/Theme';
 import Pdf from 'react-native-pdf';
 import {
-  responsiveWidth as rw,
   responsiveHeight as rh,
 } from '@/utils/responsive';
 import { strings } from '../constants/localization';
+import { ExamIcon, Exam, ArrowLeft } from '../components/icons'
+import { TouchableOpacity } from 'react-native-gesture-handler';
+
 const Calendar = () => {
   const { token } = useContext(AuthContext);
   const { department } = useContext(ProfileContext);
   const [academicData, setAcademicData] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [type, setType] = useState(null)
   const { colors } = useTheme();
   const { theme } = useContext(ThemeContext);
+
   const getAcademicMethod = async () => {
     setLoading(true);
     let response = await getAcademic(token, department);
     if (response.error) {
       setAcademicData(null);
     } else {
+      console.log(response)
       setAcademicData(response.data);
     }
     setLoading(false);
@@ -40,38 +45,76 @@ const Calendar = () => {
     getAcademicMethod();
   }, []);
   return (
-    <View style={[styles.container, { backgroundColor: colors.headerBg }]}>
-      {loading ? (
-        <ActivityIndicator size="large" color="white" style={{ justifyContent: 'center', flex: 1 }} />
-      ) : academicData == undefined ? (
-        <View style={{ justifyContent: 'center', flex: 1 }}>
-          {theme === 'light' ? (
-            <Image
-              source={require('@/assets/images/aybumobilelight.png')}
-              style={{ marginTop: 16, height: rh(48), alignSelf: 'center' }}
+    <View style={[styles.container, { backgroundColor: academicData.exam !== null ? colors.background : colors.headerBg }]}>
+      {loading
+        ? <ActivityIndicator size="large" color="black" style={{ justifyContent: 'center', flex: 1 }} />
+        : academicData == undefined ?
+          <View style={{ justifyContent: 'center', flex: 1 }}>
+            {theme === 'light' ? (
+              <Image
+                source={require('@/assets/images/aybumobilelight.png')}
+                style={{ marginTop: 16, height: rh(48), alignSelf: 'center' }}
+              />
+            ) : (
+              <Image
+                source={require('@/assets/images/aybumobiledark.png')}
+                style={{ marginTop: 16, height: rh(48), alignSelf: 'center' }}
+              />
+            )}
+            <Text
+              style={{
+                marginHorizontal: 36,
+                textAlign: 'center',
+                color: 'white',
+                marginTop: 12,
+              }}>
+              {department} {strings.calendarString1}{'\n'}{strings.calendarString2}
+            </Text>
+          </View>
+          :
+          academicData.exam !== null
+            ?
+            type == null ?
+              <View style={{ flex: 1 }}>
+                <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+                  <TouchableOpacity onPress={() => { setType(0) }} activeOpacity={.8}>
+                    <Image source={require('../assets/images/exam.png')} style={styles.image} />
+                  </TouchableOpacity>
+                  <Text style={{
+                    alignSelf: 'center',
+                    marginTop: 4,
+                    color: '#001A43',
+                    fontSize: 24
+                  }}>{strings.examSchedule}</Text>
+                </View>
+                <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+                  <TouchableOpacity onPress={() => { setType(1) }} activeOpacity={.8}>
+                    <Image source={require('../assets/images/calendar.png')} style={styles.image} />
+                  </TouchableOpacity>
+                  <Text style={{
+                    alignSelf: 'center',
+                    marginTop: 4,
+                    color: '#001A43',
+                    fontSize: 24
+                  }}>{strings.syllabus}</Text>
+                </View>
+              </View>
+              :
+              <View>
+                <TouchableOpacity onPress={() => { setType(null) }} activeOpacity={.8} style={{ margin: 16 }}>
+                  <ArrowLeft width={24} height={24} />
+                </TouchableOpacity>
+                <Pdf
+                  source={{ uri: `data:application/pdf;base64,${academicData.content}` }}
+                  style={styles.pdf}
+                />
+              </View>
+            :
+            <Pdf
+              source={{ uri: `data:application/pdf;base64,${type == 0 ? academicData.exam : academicData.content}` }}
+              style={styles.pdf}
             />
-          ) : (
-            <Image
-              source={require('@/assets/images/aybumobiledark.png')}
-              style={{ marginTop: 16, height: rh(48), alignSelf: 'center' }}
-            />
-          )}
-          <Text
-            style={{
-              marginHorizontal: 36,
-              textAlign: 'center',
-              color: 'white',
-              marginTop: 12,
-            }}>
-            {department} {strings.calendarString1}{'\n'}{strings.calendarString2}
-          </Text>
-        </View>
-      ) : (
-        <Pdf
-          source={{ uri: `data:application/pdf;base64,${academicData.content}` }}
-          style={styles.pdf}
-        />
-      )}
+      }
     </View>
   );
 };
@@ -86,6 +129,10 @@ const styles = StyleSheet.create({
     flex: 1,
     width: Dimensions.get('window').width,
     height: Dimensions.get('window').height,
+  },
+  image: {
+    width: 100,
+    height: 100,
   },
 });
 export default Calendar;
